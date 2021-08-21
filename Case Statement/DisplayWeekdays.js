10549ac04921a0f9b2552584201864502b645cef
vivek <vivek.iitd93@gmail.com>
2021-08-21 08:01:39 +0530

    Solve probelm related case statement

---
+++ Case Statement/DisplayWeekdays.js
@@ -0,0 +1,26 @@
+let singleDigitNumber = (Math.floor(Math.random() * 10) % 7) + 1;
+switch (singleDigitNumber) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("wrong number");
+}
